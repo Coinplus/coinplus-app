a98cb2ec7b8c0d6530c3754a0e3e21715c266d4f
Aram Bayadyan <aram.bv@gmail.com>
2023-06-04 22:52:25 +0400

    ndef json payload parsing

--- lib/pages/onboarding_page/onboarding_page.dart
+++ lib/pages/onboarding_page/onboarding_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_scale_tap/flutter_scale_tap.dart';
 import 'package:gap/gap.dart';
 import 'package:lottie/lottie.dart';
